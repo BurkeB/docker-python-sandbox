e1ca375092dde29a7a922dab4793298ad9ba9e85
Bruno Burke <burke@fh-muenster.de>
2017-04-13 17:01:23 +0200

    separated the image from the library

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-IMAGE_NAME="christophetd/docker_sandbox"
+IMAGE_NAME="docker-python-sandbox-image"
 
 if [ $# = 1 ]; then
 	IMAGE_NAME="$1"
 fi
 
 echo "Building image '$IMAGE_NAME'..."
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME ./container
